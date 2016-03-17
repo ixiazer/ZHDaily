4e896edd164fa6b2b7902dfeb1d4732c3e151e6c
ixiazer <shan.xu@freshfresh.com>
2016-03-17 17:58:05 +0800

    Initial Commit

---
+++ ZHDaily/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZHDaily
+//
+//  Created by ixiazer on 16/3/17.
+//  Copyright © 2016年 FF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
